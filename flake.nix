44d375b9086f8a019245f355b21b20fd9efa6a69
Graham Christensen <graham@grahamc.com>
2022-02-22 14:10:54 -0500

    Init a flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "NixOS integration for Vault-backed secrets on systemd services.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs
+    , self
+    , ...
+    }@inputs:
+    let
+      nameValuePair = name: value: { inherit name value; };
+      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+
+      pkgsFor = pkgs: system:
+        import pkgs { inherit system; };
+
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
+      forAllSystems = f: genAttrs allSystems
+        (system: f {
+          inherit system;
+          pkgs = pkgsFor nixpkgs system;
+        });
+    in
+    {
+      devShell = forAllSystems
+        ({ pkgs, ... }:
+          pkgs.mkShell {
+            buildInputs = with pkgs; [
+              vault
+            ];
+          }
+        );
+    };
+}

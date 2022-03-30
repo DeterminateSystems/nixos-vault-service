f8a820f0f97a5ae0b8cc4e11375d64e8a6f9cc54
Cole Helbling <cole.helbling@determinate.systems>
2022-03-30 12:10:51 -0700

    tests: test and support default agent configuration

--- module/implementation.tests.nix
+++ module/implementation.tests.nix
@@ -539,4 +539,58 @@ in
       if "dead" not in machine.succeed("systemctl show -p SubState --value example"):
           raise Exception("unit shouldn't have even started if the sidecar unit failed")
     '';
+
+  defaultConfig = mkTest
+    ({ pkgs, ... }: {
+      detsys.defaultAgentConfig = {
+        vault = [{ address = "http://127.0.0.1:8200"; }];
+        auto_auth = [{
+          method = [{
+            config = [{
+              remove_secret_id_file_after_reading = false;
+              role_id_file_path = "/role_id";
+              secret_id_file_path = "/secret_id";
+            }];
+            type = "approle";
+          }];
+        }];
+        template_config = [{
+          static_secret_render_interval = "5s";
+        }];
+      };
+
+      detsys.systemd.services.example.vaultAgent = {
+        secretFiles.files."rand_bytes".template = ''
+          {{ with secret "sys/tools/random/3" "format=base64" }}
+          Have THREE random bytes from a templated string! {{ .Data.random_bytes }}
+          {{ end }}
+        '';
+
+        secretFiles.files."rand_bytes-v2".templateFile =
+          let
+            file = pkgs.writeText "rand_bytes-v2.tpl" ''
+              {{ with secret "sys/tools/random/6" "format=base64" }}
+              Have SIX random bytes, but from a template file! {{ .Data.random_bytes }}
+              {{ end }}
+            '';
+          in
+          file;
+      };
+
+      systemd.services.example = {
+        script = ''
+          cat /tmp/detsys-vault/rand_bytes
+          cat /tmp/detsys-vault/rand_bytes-v2
+          sleep infinity
+        '';
+      };
+    })
+    ''
+      machine.wait_for_file("/secret_id")
+      machine.start_job("example")
+      machine.wait_for_job("detsys-vaultAgent-example")
+      print(machine.succeed("systemd-run -p JoinsNamespaceOf=detsys-vaultAgent-example.service -p PrivateTmp=true cat /tmp/detsys-vault/rand_bytes"))
+      print(machine.succeed("systemd-run -p JoinsNamespaceOf=detsys-vaultAgent-example.service -p PrivateTmp=true cat /tmp/detsys-vault/rand_bytes-v2"))
+      print(machine.succeed("journalctl -u detsys-vaultAgent-example"))
+    '';
 }
